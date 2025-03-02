a9f97e9d423895a39c5f26b75dbb4e8ff18d3d5d
user <ruslano48@yandex.ru>
2025-03-02 19:11:05 +0300

    Chess

---
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -0,0 +1,52 @@
+package ru.job4j.chess.firuges.black;
+
+import ru.job4j.chess.ImpossibleMoveException;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.Figure;
+
+public class BishopBlack implements Figure {
+    private final Cell position;
+
+    public BishopBlack(final Cell ps) {
+        position = ps;
+    }
+
+    @Override
+    public Cell position() {
+        return position;
+    }
+
+    @Override
+    public Cell[] way(Cell dest) {
+        if (!isDiagonal(position, dest)) {
+            throw new ImpossibleMoveException(
+                    String.format("Could not move by diagonal from %s to %s", position, dest));
+
+        }
+
+        int deltaX = Integer.compare(position.getX(), dest.getX());
+        int deltaY = Integer.compare(position.getY(), dest.getY());
+
+        int size = Math.abs(dest.getX() - position.getX());
+        Cell[] steps = new Cell[size];
+        int x = position.getX();
+        int y = position.getY();
+
+        for (int index = 0; index < size; index++) {
+            x += deltaX;
+            y += deltaY;
+
+            steps[index] = Cell.findBy(x, y);
+        }
+        return steps;
+    }
+
+    public boolean isDiagonal(Cell source, Cell dest) {
+        return Math.abs(source.getX() - dest.getX()) == Math.abs(source.getY() - dest.getY());
+    }
+
+    @Override
+    public Figure copy(Cell dest) {
+        return new BishopBlack(dest);
+    }
+}
